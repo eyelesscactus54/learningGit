0e24ae5d76ea1fabcc98a7949a3de4948623a0e1
eyelesscactus54 <cyprianblunt@outlook.com>
2018-09-12 11:25:58 -0400

    Add an author/email comment

--- hello.rb
+++ hello.rb
@@ -1,5 +1,6 @@
 #Default is "World"
 #Name is eyelesscactus54, reset some stuff
+# Author: Cyprian Blunt, aka eyelesscactus54 (cyprianblunt@outlook.com)
 name = ARGV.first || "World"
 
 puts "Hello, #{name}!"
